2226ac01196669c00d4ca7ed1cda83a49351e19c
ccunning2 <cameron.cunning@huskers.unl.edu>
2017-04-23 12:39:45 -0500

    Added some output in slave

--- src/jvm/backtype/storm/scheduler/Elasticity/Slave.java
+++ src/jvm/backtype/storm/scheduler/Elasticity/Slave.java
@@ -10,9 +10,11 @@ public class Slave {
 	public static Profile prf;
 	
 	public static void main (String[] args) throws InterruptedException, IOException{
-	//public static void start () throws UnknownHostException, InterruptedException{	
-		prf=new Profile(InetAddress.getLocalHost().getHostAddress());
+	//public static void start () throws UnknownHostException, InterruptedException{
+        System.out.println("In main...");
+        prf=new Profile(InetAddress.getLocalHost().getHostAddress());
 		Thread t=new Thread(new ProfileUpdate());
+        System.out.println("Starting profile update thread");
 		t.start();
 		while(true){
 			Thread.sleep(1000);
@@ -34,7 +36,8 @@ class SlaveWorker implements Runnable{
 			
 			//prf.examine();
 			//send service info
-			Socket socket = new Socket("10.71.104.210",6789);
+            System.out.println("In worker run, opening connection...");
+            Socket socket = new Socket("10.71.104.210",6789);
 			ObjectOutputStream out=new ObjectOutputStream(socket.getOutputStream());
 			out.flush();
 			out.writeObject(Slave.prf.ip);
@@ -42,8 +45,8 @@ class SlaveWorker implements Runnable{
 			out.writeObject(Slave.prf.getBandwidth_in());
 			out.writeObject(Slave.prf.getBandwidth_out());
 			out.flush();
-			
-		} catch (UnknownHostException e) {
+            System.out.println("Completed transmission!");
+        } catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
